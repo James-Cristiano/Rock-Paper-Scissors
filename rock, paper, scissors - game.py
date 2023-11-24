aae50c7c67cd0d3024ca768fc321c2e684e45d25
James Cristiano <146529916+James-Cristiano@users.noreply.github.com>
2023-11-24 21:40:04 +1100

    Add files via upload
    Cleaned up version of the game

--- rock, paper, scissors - game.py
+++ rock, paper, scissors - game.py
@@ -30,17 +30,6 @@ while True:
         else:
             print('Please enter one of the following options to paly: R, P, S or Q to quit. \nTry again!\n\n')
 
-
-#TODO:Creating an all or nothing mode
-        #if Points >= 1:
-            #print('Would you like to go all or nothing? Risking all of your points can score you x2 what you have now!')
-            #print('But if you lose... You will have to start all over again.\n\nCare to Try?\n(Y)es or (N)o?')
-            #DoubleDown = input('>> ').upper()
-            #if DoubleDown == 'N':
-                #break
-            #if DoubleDown == 'Y':
-                #print('Let\'s get ready to rumbleeee!')
-
 #Confirm player choice
     if PlayerChoice == 'R':
         print('ROCK VS ...')
@@ -52,25 +41,14 @@ while True:
         print('SCISSORS VS ...')
         PlayerChoice = 'SCISSORS'        
 
-#Countdown timer?
-    #time.sleep(1)
+#Countdown timer - giving a little suspense
+    time.sleep(0.3)
     print('...1')
-    #time.sleep(1)
+    time.sleep(0.3)
     print('...2')
-    #time.sleep(1)
+    time.sleep(0.3)
     print('...3')
-    #time.sleep(0.5)
-    
-#fixing double down
-    #while Points >= 1: 
-        #print('Before we reveal the answer, would you like to go all or nothing? Risking all of your points can score you x2 what you have now!')
-        #print('But if you lose... You will have to start all over again.\n\nCare to Try?\n(Y)es or (N)o?')
-        #DoubleDown = input('>> ').upper()
-        #if DoubleDown == 'N':
-            #break
-        #if DoubleDown == 'Y':
-            #print('Let\'s get ready to rumbleeee!')
-        #break
+    time.sleep(0.5)
 
 #Get PC to make a choice
     ComputerChoice = random.randint(1, 3)
@@ -82,56 +60,37 @@ while True:
         ComputerChoice = 'SCISSORS'
     print(ComputerChoice)
 
-#Record your stats
-    #DoubleDown = DoublePoints
+#Declaring the win/lose conditions and recording your states and points
     if PlayerChoice == ComputerChoice:
         print('I guess we have a Tie!')
         Ties = Ties + 1
     elif PlayerChoice == 'ROCK' and ComputerChoice == 'SCISSORS':
         print('You are the winner!')
         Wins = Wins + 1
-        #if DoubleDown == 'Y':
-            #Points = Points * 2
-        #if DoubleDown == 'N': 
         Points = Points + 1
     elif PlayerChoice == 'PAPER' and ComputerChoice == 'ROCK':
         print('You are the winner!')
-        Wins = Wins + 1
-        #if DoubleDown == 'Y':
-            #Points = Points * 2
-        #if DoubleDown == 'N': 
+        Wins = Wins + 1 
         Points = Points + 1
     elif PlayerChoice == 'SCISSORS' and ComputerChoice == 'PAPER':
         print('You are the winner!')
         Wins = Wins + 1
-        #if DoubleDown == 'Y':
-            #Points = Points * 2
-        #if DoubleDown == 'N': 
         Points = Points + 1
     elif PlayerChoice == 'ROCK' and ComputerChoice == 'PAPER':
         print('Better luck next time! You lose.')
         Losses = Losses + 1
-        #if DoubleDown == 'Y':
-            #Points = 0
-        #elif DoubleDown == 'N': 
         Points = Points - 1
         if Points <= 0:
             Points = 0
     elif PlayerChoice == 'PAPER' and ComputerChoice == 'SCISSORS':
         print('Better luck next time! You lose.')
         Losses = Losses + 1
-        #if DoubleDown == 'Y':
-            #Points = 0
-        #elif DoubleDown == 'N': 
         Points = Points - 1
         if Points <= 0:
             Points = 0
     elif PlayerChoice == 'SCISSORS' and ComputerChoice == 'ROCK':
         print('Better luck next time! You lose.')
         Losses = Losses + 1
-        #if DoubleDown == 'Y':
-            #Points = 0
-        #elif DoubleDown == 'N': 
         Points = Points - 1
         if Points <= 0:
             Points = 0
